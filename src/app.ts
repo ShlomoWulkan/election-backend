2372b2d2b79f2124308c82ec2c71ade4af10e1b8
shlomo <shlomo@dev.com>
2024-11-10 11:49:28 +0200

    controller files and adding to middleware for app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express  from "express";
 import 'dotenv/config'
+import userController from "./controllers/userController";
+import adminController from "./controllers/adminController";
+import votesController from "./controllers/votesController";
+import candidateController from "./controllers/candidateController";
 
 const port = process.env.PORT || 2224
 const app = express();
+
+app.use(express.json())
+
+app.use("/api/users", userController)
+app.use("/api/admin", adminController)
+app.use("/api/votes", votesController)
+app.use("/api/candidates", candidateController)
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
---
+++ src/controllers/votesController.ts
@@ -0,0 +1,5 @@
+import e, { Router } from "express";
+
+const router = Router();
+
+export default router
